a3d1ac3632c4b058340aaf8a1f9d96332517fe26
Francisco Ramini <framini@gmail.com>
2016-06-08 16:37:14 -0300

    update the public API unit test

--- test/unit/public-api.js
+++ test/unit/public-api.js
@@ -1,10 +1,12 @@
 import test from 'tape';
-import dataapi from '../../src/dataapi';
+import responsiveState from '../../src/responsivestate';
 
 test('public API matches expectaction', t => {
-  const da = dataapi();
-  t.equal(typeof da.stop, 'function', 'stop is a method');
-  t.equal(typeof da.start, 'function', 'start is a method');
-  t.equal(typeof da.getInitializedComponents, 'function', 'getInitializedComponents is a method');
+  const rs = responsiveState();
+  t.equal(typeof rs.add, 'function', 'add is a method');
+  t.equal(typeof rs.remove, 'function', 'remove is a method');
+  t.equal(typeof rs.removeAll, 'function', 'removeAll is a method');
+  t.equal(typeof rs.getState, 'function', 'getState is a method');
+  t.equal(typeof rs.getStates, 'function', 'getStates is a method');
   t.end();
 });
